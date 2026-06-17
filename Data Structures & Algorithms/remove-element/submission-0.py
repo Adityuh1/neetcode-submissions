364b96c180afe3ee5f8070b87e57073dd982d170
Aditya Singh Jadaon <101944703+Adityuh1@users.noreply.github.com>
2026-06-18 02:55:54 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        n = len(nums)
+        k = 0
+
+        for i in range(0,n):
+            if nums[i] != val:
+                # partition of quick sort
+                nums[k] = nums[i]
+                k+=1
+        return k
